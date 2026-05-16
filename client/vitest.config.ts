89f3641226dc149152404bb166e5a03729052588
Nina Prachárová <xpracha1@fi.muni.cz>
2026-05-16 14:53:28 +0200

    testing-library for components' tests setup

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     environment: "node",
+    setupFiles: ["./src/test-setup.ts"],
   },
   resolve: {
     alias: {
